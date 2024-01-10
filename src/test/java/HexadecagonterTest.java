0c3d34eedd3ea65f62581e23c19fd29e57ea43a1
Vito <lolozol80xd@gmail.com>
2024-01-10 08:21:08 +0100

    setup ready

--- src/test/java/HexadecagonterTest.java
+++ src/test/java/HexadecagonterTest.java
@@ -1,9 +1,18 @@
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import lan.zold.Hexadecagonter;
+
 public class HexadecagonterTest {
+
     Hexadecagonter hexadecagonter;
+
     @BeforeTest
-    
+    public void setUp(){
+        hexadecagonter = new Hexadecagonter();
+
+    }
+
+
     @Test
 }
