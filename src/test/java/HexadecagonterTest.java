bd894842377e5d4134d6d9ac4e0260907ce86f1c
Vito <lolozol80xd@gmail.com>
2024-01-10 08:18:17 +0100

    start

---
+++ src/test/java/HexadecagonterTest.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class HexadecagonterTest {
+    Hexadecagonter hexadecagonter;
+    @BeforeTest
+    
+    @Test
+}
