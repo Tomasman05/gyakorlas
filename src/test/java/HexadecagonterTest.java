bd0a8e35adc83b73628ffd666288571b15744678
Vito <lolozol80xd@gmail.com>
2024-01-10 08:44:59 +0100

    test4

--- src/test/java/HexadecagonterTest.java
+++ src/test/java/HexadecagonterTest.java
@@ -17,13 +17,13 @@ public class HexadecagonterTest {
 
     @Test
     public void testCalcArea_30(){
-        double actual = hexadecagonter.calcArea(30);
+        double actual = hexadecagonter.calcArea(30.0);
         double expected = 18098.422;
         Assert.assertEquals(actual, expected,0.1);
     }
     @Test
     public void testCalcArea_20(){
-        double actual = hexadecagonter.calcArea(20);
+        double actual = hexadecagonter.calcArea(20.0);
         double expected = 8043.743;
         Assert.assertEquals(actual, expected,0.1);
     }
